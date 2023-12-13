b24f2206fdcc06148edb6ec22d1462648f8cd876
ManoKondz <egabrielrbm@gmail.com>
2023-12-13 12:45:55 -0300

    áudio da urna

--- UrnaEletronica/src/main/java/br/edu/telas/TelaFim.java
+++ UrnaEletronica/src/main/java/br/edu/telas/TelaFim.java
@@ -1,9 +1,35 @@
 package br.edu.telas;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
 public class TelaFim {
 
     private void tocarMusica() {
+        // Toca o som da urna
+        playUrnaSound();
 
+        // Fecha a tela final da urna
+        closeTelaFinal();
+    }
+
+    private static void playUrnaSound() {
+        try {
+            URL soundFile = TelaFim.class.getResource("/caminho/do/seu/arquivo/urna_sound.wav");
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeTelaFinal() {
+        // Implemente o código para fechar a tela final da urna
+    }
     }
 
 }
